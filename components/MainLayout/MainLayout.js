2d20955baf05ec40b74ec3a97a99e06f85ba9a89
Denis Shevchenko <DAlShevchenko@sberbank.ru>
2020-07-08 20:06:46 +0300

    Added styles in MainLayout@

--- components/MainLayout/MainLayout.js
+++ components/MainLayout/MainLayout.js
@@ -1,14 +1,16 @@
 import Head from "next/head";
 import Link from "next/link";
 
+import styles from "./MainLayout.module.css";
+
 export default function MainLayout({ children, title = "My best App" }) {
   return (
-    <>
+    <div className={styles.wrapper}>
       <Head>
         <title>{title}</title>
         <meta charSet="utf-8" />
       </Head>
-      <nav>
+      <nav className={styles.navigation}>
         <Link href="/">
           <a>Home</a>
         </Link>
@@ -19,7 +21,7 @@ export default function MainLayout({ children, title = "My best App" }) {
           <a>Compant</a>
         </Link>
       </nav>
-      <main>{children}</main>
-    </>
+      <main className={styles.main}>{children}</main>
+    </div>
   );
 }
